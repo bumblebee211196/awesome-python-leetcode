6ddd43bf3c20fae1576a9bfb993340480b4c5cd7
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 17:27:41 +0530

    :sparkles: Add solution and testcases to problem 35

--- src/_0035_search_insert_position/solution.py
+++ src/_0035_search_insert_position/solution.py
@@ -1,3 +1,18 @@
-def solution():
-return
+from typing import List
+
+
+def solution(nums: List[int], target: int) -> int:
+    n = len(nums)
+    l, r = 0, n - 1
+    while l < r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            return m
+        if nums[m] < target:
+            l = m + 1
+        else:
+            r = m
+    if target > nums[l]:
+        l += 1
+    return l
     
--- tests/test_0035_search_insert_position.py
+++ tests/test_0035_search_insert_position.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0035_search_insert_position.solution import solution
+
 
 class TestSolution:
 
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize("nums, target, result", [
+        ([1,3,5,6], 3, 1),
+        ([1,3,5,6], 2, 1),
+        ([1,3,5,6], 7, 4),
+        ([1,3,5,6], 0, 0),
+        ([1], 0, 0),
+    ])
+    def test_solution(self, nums, target, result):
+        assert solution(nums, target) == result
