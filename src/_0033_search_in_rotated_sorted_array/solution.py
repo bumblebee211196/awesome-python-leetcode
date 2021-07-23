213eec6d62ee93a6b89e95d1e180b91008ce4a49
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 16:55:24 +0530

    :sparkles: Add solution and testcases to problem 33

--- src/_0033_search_in_rotated_sorted_array/solution.py
+++ src/_0033_search_in_rotated_sorted_array/solution.py
@@ -1,3 +1,19 @@
-def solution():
-return
-    
+from typing import List
+
+def solution(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            return m
+        elif nums[m] >= nums[l]:
+            if target >= nums[l] and target <= nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        else:
+            if target >= nums[m] and target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+    return -1
--- tests/test_0033_search_in_rotated_sorted_array.py
+++ tests/test_0033_search_in_rotated_sorted_array.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0033_search_in_rotated_sorted_array.solution import solution
+
 
 class TestSolution:
 
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize("nums, target, result", [
+        ([4,5,6,7,0,1,2], 0, 4),
+        ([4,5,6,7,0,1,2], 2, 6),
+        ([4,5,6,0,1,2,3], 5, 1),
+        ([4,5,6,0,1,2,3], 2, 5),
+        ([4,5,6,7,0,1,2], 3, -1),
+        ([1], 0, -1),
+    ])
+    def test_solution(self, nums, target, result):
+        assert solution(nums, target) == result
