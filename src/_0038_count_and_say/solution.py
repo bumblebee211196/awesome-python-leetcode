d5f2934f0fd887b05a7845bbce1b47ed1ac1255c
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 22:01:00 +0530

    :sparkles: Add solution and testcases to problem 38

--- src/_0038_count_and_say/solution.py
+++ src/_0038_count_and_say/solution.py
@@ -1,3 +1,18 @@
-def solution():
-return
-    
+def solution(n: int) -> str:
+    pattern = '11'
+    if n == 1:
+        return '1'
+    if n == 2:
+        return '11'
+    for _ in range(n-2):
+        count = 1
+        temp = ""
+        for i in range(len(pattern)-1):
+            if pattern[i] == pattern[i+1]:
+                count += 1
+            else:
+                temp += f"{count}{pattern[i]}"
+                count = 1
+        temp += f"{count}{pattern[-1]}"
+        pattern = temp
+    return pattern
--- tests/test_0038_count_and_say.py
+++ tests/test_0038_count_and_say.py
@@ -1,6 +1,12 @@
 import pytest
+from src._0038_count_and_say.solution import solution
 
 class TestSolution:
 
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize("nums, result", [
+        (1, "1"),
+        (2, "11"),
+        (4, "1211"),
+    ])
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
