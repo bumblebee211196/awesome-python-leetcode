76cf06cd560ffb075a9f644f7ed6cc303ba6c31b
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 22:41:45 +0530

    :sparkles: Add solution and testcases to problem 42

--- src/_0042_trapping_rain_water/solution.py
+++ src/_0042_trapping_rain_water/solution.py
@@ -1,3 +1,19 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(arr: List[int]) -> int:
+    n = len(arr)
+    if n < 3:
+        return 0
+    total = 0
+    i, j = 0, n - 1
+    l_max, r_max = arr[i], arr[j]
+    while i <= j:
+        if l_max < r_max:
+            total += l_max - arr[i]
+            i += 1
+        else:
+            total += r_max - arr[j]
+            j -= 1
+        l_max, r_max = max(l_max, arr[i]), max(r_max, arr[j])
+    return total
--- tests/test_0042_trapping_rain_water.py
+++ tests/test_0042_trapping_rain_water.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0042_trapping_rain_water.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "arr, result",
+        [
+            ([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
+            ([4, 2, 0, 3, 2, 5], 9),
+            ([1, 2], 0),
+        ],
+    )
+    def test_solution(self, arr, result):
+        assert solution(arr) == result
