5fa71022643e7ebf6e3197e854228e577dd5ba7a
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 22:14:59 +0530

    :sparkles: Add solution and testcases to problem 39

--- src/_0039_combination_sum/solution.py
+++ src/_0039_combination_sum/solution.py
@@ -1,3 +1,15 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(candidates: List[int], target: int) -> List[List[int]]:
+    dp = [[] for _ in range(target + 1)]
+    for c in candidates:
+        for i in range(1, target + 1):
+            if i < c:
+                continue
+            if i == c:
+                dp[i].append([c])
+            else:
+                for val in dp[i - c]:
+                    dp[i].append(val + [c])
+    return dp[-1]
