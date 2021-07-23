e7efbc7f4f5908b2c959c2861a6180b9f90aac10
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 17:33:19 +0530

    :sparkles: Add solution and testcases to problem 36

--- src/_0036_valid_sudoku/solution.py
+++ src/_0036_valid_sudoku/solution.py
@@ -1,3 +1,29 @@
-def solution():
-return
+from typing import List
+
+
+def solution(board: List[List[str]]) -> bool:
+    for row in board:
+        values = [value for value in row if value != "."]
+        if len(values) != len(set(values)):
+            return False
+    
+    for col in zip(*board):
+        values = [value for value in col if value != "."]
+        if len(values) != len(set(values)):
+            return False
+    
+    for i in range(3):
+        for j in range(3):
+            if not is_valid_box(i, j, board):
+                return False
+    
+    return True
+
+def is_valid_box(r, c, board) -> bool:
+    values = []
+    for i in range(r * 3, (r + 1) * 3):
+        for j in range(c * 3, (c + 1) * 3):
+            if board[i][j] != ".":
+                values.append(board[i][j])
+    return len(values) == len(set(values))
     
