5fd7e1b049e6a1b118117c56cca540d5cef487e9
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 17:27:32 +0530

    :sparkles: Add solution and testcases to problem 34

--- src/_0034_find_first_and_last_position_of_element_in_sorted_array/solution.py
+++ src/_0034_find_first_and_last_position_of_element_in_sorted_array/solution.py
@@ -1,3 +1,28 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(nums: List[int], target: int) -> List[int]:
+    n = len(nums)
+    index = -1
+    l, r = 0, n - 1
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            index = m
+        if nums[m] >= target:
+            r = m - 1
+        else:
+            l = m + 1
+    lres = index
+    index = -1
+    l, r = 0, n - 1
+    while l <= r:
+        m = (l + r) // 2
+        if nums[m] == target:
+            index = m
+        if nums[m] <= target:
+            l = m + 1
+        else:
+            r = m - 1
+    rres = index
+    return [lres, rres]
--- tests/test_0034_find_first_and_last_position_of_element_in_sorted_array.py
+++ tests/test_0034_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,6 +1,12 @@
 import pytest
+from src._0034_find_first_and_last_position_of_element_in_sorted_array.solution import solution
 
 class TestSolution:
 
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize("nums, target, result", [
+        ([5,7,7,8,8,10], 8, [3,4]),
+        ([5,7,7,8,8,10], 6, [-1,-1]),
+        ([], 0, [-1,-1]),
+    ])
+    def test_solution(self, nums, target, result):
+        assert solution(nums, target) == result
