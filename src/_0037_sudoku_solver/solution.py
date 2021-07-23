ae8721a3cad40284d51fc350221a1e67314a5262
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 17:47:12 +0530

    :sparkles: Add solution and testcases to problem 37

--- src/_0037_sudoku_solver/solution.py
+++ src/_0037_sudoku_solver/solution.py
@@ -1,3 +1,41 @@
-def solution():
-return
+from typing import List
+
+
+def solution(board: List[List[str]]):
+    def is_valid(i, j, v):
+        # Check column
+        for c in range(N):
+            if c != j and board[i][c] == v:
+                return False
+        # Check row
+        for r in range(N):
+            if r != i and board[r][j] == v:
+                return False
+        # Check 3 X 3 board
+        rs = (i // 3) * 3
+        cs = (j // 3) * 3
+        for r in range(rs, rs + 3):
+            for c in range(cs, cs + 3):
+                if r != i and c != j and board[r][c] == v:
+                    return False
+        return True
+    
+    def solve():
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == ".":
+                    for v in range(1, 10):
+                        v = str(v)
+                        if is_valid(i, j, v):
+                            board[i][j] = v
+                            if solve():
+                                return True
+                            else:
+                                board[i][j] = "."
+                    return False
+        return True
+    
+    N = 9
+    solve()
+    return board
     
