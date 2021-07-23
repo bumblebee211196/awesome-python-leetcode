d71dd2491dea2a05db2e6ae7f81684bed0d650cd
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-23 22:29:56 +0530

    :sparkles: Add solution and testcases to problem 40

--- src/_0040_combination_sum_ii/solution.py
+++ src/_0040_combination_sum_ii/solution.py
@@ -1,3 +1,20 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(candidates: List[int], target: int) -> List[List[int]]:
+    def helper(idx, nums, total):
+        if total == target:
+            nonlocal res
+            res.append(nums)
+            return
+        if total > target:
+            return
+        for i in range(idx, n):
+            if i == idx or candidates[i] != candidates[i - 1]:
+                helper(i + 1, nums + [candidates[i]], total + candidates[i])
+
+    n = len(candidates)
+    res = []
+    candidates.sort()
+    helper(0, [], 0)
+    return res
--- tests/test_0040_combination_sum_ii.py
+++ tests/test_0040_combination_sum_ii.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0040_combination_sum_ii.solution import solution
+
 
 class TestSolution:
-
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "candidates, target, result",
+        [
+            ([10, 1, 2, 7, 6, 1, 5], 8, [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]]),
+            ([2, 5, 2, 1, 2], 5, [[1, 2, 2], [5]]),
+        ],
+    )
+    def test_solution(self, candidates, target, result):
+        assert solution(candidates, target) == result
