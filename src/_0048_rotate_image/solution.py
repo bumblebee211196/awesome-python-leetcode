9a9c73ca652b274db89dba3580f1a9878229f6a0
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 21:09:36 +0530

    :sparkles: Add solution and testcases to problem 48

--- src/_0048_rotate_image/solution.py
+++ src/_0048_rotate_image/solution.py
@@ -1,3 +1,10 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(matrix: List[List[int]]) -> None:
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    for i, row in enumerate(matrix):
+        matrix[i] = list(reversed(row))
--- tests/test_0048_rotate_image.py
+++ tests/test_0048_rotate_image.py
@@ -1,6 +1,20 @@
 import pytest
+from src._0048_rotate_image.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "matrix, result",
+        [
+            ([[1]], [[1]]),
+            ([[1, 2], [3, 4]], [[3, 1], [4, 2]]),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
+            (
+                [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]],
+                [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]],
+            ),
+        ],
+    )
+    def test_solution(self, matrix, result):
+        solution(matrix)
+        assert matrix == result
