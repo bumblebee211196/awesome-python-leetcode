6ec81154a4fe530f7f0b4c04761f2526598a292e
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 21:20:52 +0530

    :sparkles: Add solution and testcases to problem 49

--- src/_0049_group_anagrams/solution.py
+++ src/_0049_group_anagrams/solution.py
@@ -1,3 +1,10 @@
-def solution():
-return
-    
+from typing import List
+from collections import defaultdict
+
+
+def solution(strs: List[str]) -> List[List[str]]:
+    res = defaultdict(list)
+    for word in strs:
+        key = "".join(sorted(word))
+        res[key].append(word)
+    return list(res.values())
--- tests/test_0049_group_anagrams.py
+++ tests/test_0049_group_anagrams.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0049_group_anagrams.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "strs, result",
+        [
+            (["eat", "tea", "tan", "ate", "nat", "bat"], [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]),
+            ([""], [[""]]),
+            (["a"], [["a"]]),
+        ],
+    )
+    def test_solution(self, strs, result):
+        assert solution(strs) == result
