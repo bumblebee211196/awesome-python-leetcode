cde43c9e08e055aeb2b773e58863ff7020d7fbd2
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 10:33:45 +0530

    :sparkles: Add solution and testcases to problem 46

--- src/_0046_permutations/solution.py
+++ src/_0046_permutations/solution.py
@@ -1,3 +1,19 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(nums: List[int]) -> List[List[int]]:
+    def helper(nums_, res_):
+        if not nums_:
+            nonlocal res
+            res.append(res_[:])
+            return
+        for val in nums_:
+            temp = nums_[:]
+            temp.remove(val)
+            res_.append(val)
+            helper(temp, res_)
+            res_.pop()
+
+    res = []
+    helper(nums, [])
+    return res
--- tests/test_0046_permutations.py
+++ tests/test_0046_permutations.py
@@ -1,6 +1,18 @@
 import pytest
+from src._0046_permutations.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            (
+                [1, 2, 3],
+                [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]],
+            ),
+            ([0, 1], [[0, 1], [1, 0]]),
+            ([1], [[1]]),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
