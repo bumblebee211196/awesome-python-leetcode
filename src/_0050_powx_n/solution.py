79f6f854d8a87232bbe4bd71f76fb2cd3e265adb
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 21:26:03 +0530

    :sparkles: Add solution and testcases to problem 50

--- src/_0050_powx_n/solution.py
+++ src/_0050_powx_n/solution.py
@@ -1,3 +1,13 @@
-def solution():
-return
+def solution(x: float, n: int) -> float:
+        
+    def helper(p):
+        if p == 0:
+            return 1
+        if p % 2 == 0:
+            temp = helper(p // 2)
+            return temp * temp
+        else:
+            return x * helper(p - 1)
     
+    res = helper(abs(n))
+    return res if n >= 0 else 1 / res
--- tests/test_0050_powx_n.py
+++ tests/test_0050_powx_n.py
@@ -1,6 +1,13 @@
 import pytest
+from src._0050_powx_n.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+
+    @pytest.mark.parametrize("x, n, result", [
+        (2.0, -2, 0.25),
+        (2.0, 10, 1024.0),
+        (2.1, 3, 9.261000000000001),
+    ])
+    def test_solution(self, x, n, result):
+        assert solution(x, n) == result
