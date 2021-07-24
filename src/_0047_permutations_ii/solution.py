c75dc283d7daf57aaed1b645a99bb039dc037770
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 20:50:13 +0530

    :sparkles: Add solution and testcases to problem 47

--- src/_0047_permutations_ii/solution.py
+++ src/_0047_permutations_ii/solution.py
@@ -1,3 +1,19 @@
-def solution():
-return
+from typing import List
+
+
+def solution(nums: List[int]) -> List[List[int]]:
+        
+    def helper(nums_, res_):
+        if not nums_:
+            res.append(res_)
+            return
+        for i, num in enumerate(nums_):
+            if i > 0 and nums_[i] == nums_[i - 1]:
+                continue
+            else:
+                helper(nums_[:i] + nums_[i + 1:], res_ + [num])
     
+    res = []
+    nums.sort()
+    helper(nums, [])
+    return res
--- tests/test_0047_permutations_ii.py
+++ tests/test_0047_permutations_ii.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0047_permutations_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([1, 1, 2], [[1, 1, 2], [1, 2, 1], [2, 1, 1]]),
+            ([1, 2, 3], [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
