54bda8fe5f28431714665cc88e2512f643641f9a
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 08:51:25 +0530

    :sparkles: Add solution and testcases to problem 43

--- src/_0043_multiply_strings/solution.py
+++ src/_0043_multiply_strings/solution.py
@@ -1,3 +1,24 @@
-def solution():
-return
+def solution(num1: str, num2: str) -> str:
+    if "0" in (num1, num2):
+        return "0"
+    n1, n2 = len(num1), len(num2)
+    if 1 in (n1, n2):
+        num2, num1 = sorted([num1, num2], key=len)
+        carry = 0
+        res = []
+        for v1 in num1[::-1]:
+            carry, r = divmod(int(v1) * int(num2) + carry, 10)
+            res.append(str(r))
+        if carry > 0:
+            res.append(str(carry))
+        return "".join(res)[::-1]
+    num2, num1 = sorted([num1, num2], key=len)
+    carry = 0
+    res = []
+    for v1 in num1[::-1]:
+        carry, r = divmod(int(solution(v1, num2)) + carry, 10)
+        res.append(str(r))
+    if carry > 0:
+        res.append(str(carry)[::-1])
+    return "".join(res)[::-1]
     
--- tests/test_0043_multiply_strings.py
+++ tests/test_0043_multiply_strings.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0043_multiply_strings.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+
+    @pytest.mark.parametrize("num1, num2, result", [
+        ("1", "0", "0"),
+        ("2", "3", "6"),
+        ("123", "456", "56088"),
+        ("99999", "99999", "9999800001"),
+    ])
+    def test_solution(self, num1, num2, result):
+        assert solution(num1, num2) == result
