a76d84a9c40c7aa18ddd900e646acfdec8771846
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 10:04:52 +0530

    :sparkles: Add solution and testcases to problem 44

--- src/_0044_wildcard_matching/solution.py
+++ src/_0044_wildcard_matching/solution.py
@@ -1,3 +1,19 @@
-def solution():
-return
-    
+def solution(s: str, p: str) -> bool:
+    i = j = 0
+    n, m = len(s), len(p)
+    pstar = stemp = None
+    while i < n:
+        if j < m and p[j] in ("?", s[i]):
+            i += 1
+            j += 1
+        elif j < m and p[j] == "*":
+            pstar = j
+            stemp = i
+            j += 1
+        elif pstar is None:
+            return False
+        else:
+            i = stemp + 1
+            j = pstar + 1
+            stemp = i
+    return all(x == "*" for x in p[j:])
--- tests/test_0044_wildcard_matching.py
+++ tests/test_0044_wildcard_matching.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0044_wildcard_matching.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+
+    @pytest.mark.parametrize("s, p, result", [
+        ("aa", "a", False),
+        ("aa", "*", True),
+        ("cb", "?a", False),
+        ("adceb", "*a*b", True),
+        ("acdcb", "a*c?b", False),
+    ])
+    def test_solution(self, s, p, result):
+        assert solution(s, p) == result
