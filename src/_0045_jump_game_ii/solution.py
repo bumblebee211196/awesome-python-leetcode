68059edab47af3f4e72a5e8bd1f4b90774815306
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-24 10:19:16 +0530

    :sparkles: Add solution and testcases to problem 45

--- src/_0045_jump_game_ii/solution.py
+++ src/_0045_jump_game_ii/solution.py
@@ -1,3 +1,11 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(nums: List[int]) -> int:
+    curr_reach = curr_max = jumps = 0
+    for i, num in enumerate(nums[:-1]):
+        curr_max = max(curr_max, i + num)
+        if i == curr_reach:
+            jumps += 1
+            curr_reach = curr_max
+    return jumps
