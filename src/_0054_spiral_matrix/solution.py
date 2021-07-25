a8af28f561bb110e412fdb7108e8032baa5e7416
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-25 11:12:47 +0530

    :sparkles: Add solution and testcases to problem 54

--- src/_0054_spiral_matrix/solution.py
+++ src/_0054_spiral_matrix/solution.py
@@ -1,3 +1,27 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(matrix: List[List[int]]) -> List[int]:
+    if not matrix or not matrix[0]:
+        return []
+    res = []
+    y0 = 0
+    y1 = len(matrix)
+    x0 = 0
+    x1 = len(matrix[0])
+    while y1 > y0 and x1 > x0:
+        for i in range(x0, x1):
+            res.append(matrix[y0][i])
+        for j in range(y0 + 1, y1 - 1):
+            res.append(matrix[j][x1 - 1])
+        if y1 != y0 + 1:
+            for i in range(x1 - 1, x0 - 1, -1):
+                res.append(matrix[y1 - 1][i])
+        if x0 != x1 - 1:
+            for j in range(y1 - 2, y0, -1):
+                res.append(matrix[j][x0])
+        y0 += 1
+        y1 -= 1
+        x0 += 1
+        x1 -= 1
+    return res
--- tests/test_0054_spiral_matrix.py
+++ tests/test_0054_spiral_matrix.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0054_spiral_matrix.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "matrix, result",
+        [
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+            ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
+        ],
+    )
+    def test_solution(self, matrix, result):
+        assert solution(matrix) == result
