e540f49a43066b3ee254ef289e0e180ffe4c9654
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-25 11:02:23 +0530

    :sparkles: Add solution and testcases to problem 53

--- src/_0053_maximum_subarray/solution.py
+++ src/_0053_maximum_subarray/solution.py
@@ -1,3 +1,9 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(nums: List[int]) -> int:
+    curr_max = global_max = nums[0]
+    for num in nums[1:]:
+        curr_max = max(num, num + curr_max)
+        global_max = max(global_max, curr_max)
+    return global_max
--- tests/test_0053_maximum_subarray.py
+++ tests/test_0053_maximum_subarray.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0053_maximum_subarray.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+            ([1], 1),
+            ([5, 4, -1, 7, 8], 23),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
