2912a644a5ade8bb561e89c6b5d6961e3a4b3642
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-25 08:36:32 +0530

    :bug: Fix codefactor issues

--- src/_0050_powx_n/solution.py
+++ src/_0050_powx_n/solution.py
@@ -1,13 +1,11 @@
 def solution(x: float, n: int) -> float:
-        
     def helper(p):
         if p == 0:
             return 1
         if p % 2 == 0:
             temp = helper(p // 2)
             return temp * temp
-        else:
-            return x * helper(p - 1)
-    
+        return x * helper(p - 1)
+
     res = helper(abs(n))
     return res if n >= 0 else 1 / res
