5caa1a1100ba679bdc95f941dc31bd8bf5234000
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-25 11:26:26 +0530

    :sparkles: Add solution and testcases to problem 55

--- src/_0055_jump_game/solution.py
+++ src/_0055_jump_game/solution.py
@@ -1,3 +1,10 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(nums: List[int]) -> bool:
+    n = len(nums)
+    last = n - 1
+    for i in range(n - 2, -1, -1):
+        if i + nums[i] >= last:
+            last = i
+    return last == 0
--- tests/test_0055_jump_game.py
+++ tests/test_0055_jump_game.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0055_jump_game.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([2, 3, 1, 1, 4], True),
+            ([3, 2, 1, 0, 4], False),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
