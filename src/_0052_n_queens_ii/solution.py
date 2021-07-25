e6ebd63520b0ad66e6375041e04550e81e326dce
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-25 10:52:41 +0530

    :sparkles: Add solution and testcases to problem 52

--- src/_0052_n_queens_ii/solution.py
+++ src/_0052_n_queens_ii/solution.py
@@ -1,3 +1,27 @@
-def solution():
-return
-    
+def solution(n: int) -> int:
+    board = [["."] * n for _ in range(n)]
+    cols = set()
+    l_diag = set()
+    r_diag = set()
+    res = 0
+
+    def helper(row=0):
+        if row == len(board):
+            nonlocal res
+            res += 1
+            return
+        for col in range(len(board)):
+            if col in cols or row - col in l_diag or row + col in r_diag:
+                continue
+            board[row][col] = "Q"
+            cols.add(col)
+            l_diag.add(row - col)
+            r_diag.add(row + col)
+            helper(row + 1)
+            board[row][col] = "."
+            cols.remove(col)
+            l_diag.remove(row - col)
+            r_diag.remove(row + col)
+
+    helper()
+    return res
--- tests/test_0052_n_queens_ii.py
+++ tests/test_0052_n_queens_ii.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0052_n_queens_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "n, result",
+        [
+            (4, 2),
+            (1, 1),
+        ],
+    )
+    def test_solution(self, n, result):
+        assert solution(n) == result
