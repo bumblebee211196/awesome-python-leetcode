3f8f9bb5a953936406a8470a4af2f1ebf38aab04
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-26 10:19:35 +0530

    :sparkles: Add solution and testcases to problem 56

--- src/_0056_merge_intervals/solution.py
+++ src/_0056_merge_intervals/solution.py
@@ -1,3 +1,20 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(intervals: List[List[int]]) -> List[List[int]]:
+    n = len(intervals)
+    if n in (0, 1):
+        return intervals
+    intervals = sorted(intervals, key=lambda x: (x[0], x[1]))
+    res = []
+    x1, y1 = intervals[0]
+    for x2, y2 in intervals[1:]:
+        if x2 <= y1 <= y2:
+            y1 = y2
+        elif x1 <= x2 and y2 <= y1:
+            continue
+        else:
+            res.append([x1, y1])
+            x1, y1 = x2, y2
+    res.append([x1, y1])
+    return res
--- tests/test_0056_merge_intervals.py
+++ tests/test_0056_merge_intervals.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0056_merge_intervals.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "intervals, result",
+        [
+            ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
+            ([[1, 4], [4, 5]], [[1, 5]]),
+            ([[1, 4], [2, 3]], [[1, 4]]),
+            ([[1, 4]], [[1, 4]]),
+        ],
+    )
+    def test_solution(self, intervals, result):
+        assert solution(intervals) == result
