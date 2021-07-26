8fe5b3c2b0b524240e943543dacaa5f317941a33
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-26 15:13:00 +0530

    :sparkles: Add solution and testcases to problem 61

--- src/_0061_rotate_list/solution.py
+++ src/_0061_rotate_list/solution.py
@@ -1,2 +1,24 @@
-def solution():
-    return
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def solution(head: ListNode, k: int) -> ListNode:
+    if not head or not head.next:
+        return head
+    curr = head
+    n = 1
+    while curr.next:
+        n += 1
+        curr = curr.next
+    if k % n == 0:
+        return head
+    curr.next = head
+    i = 1
+    while i < n - (k % n):
+        head = head.next
+        i += 1
+    curr = head.next
+    head.next = None
+    return curr
--- tests/test_0061_rotate_list.py
+++ tests/test_0061_rotate_list.py
@@ -1,6 +1,31 @@
 import pytest
+from src._0061_rotate_list.solution import ListNode, solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "head, k, result",
+        [
+            (
+                ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+                2,
+                ListNode(4, ListNode(5, ListNode(1, ListNode(2, ListNode(3))))),
+            ),
+            (
+                ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+                10,
+                ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+            ),
+            (None, 4, None),
+        ],
+    )
+    def test_solution(self, head, k, result):
+        assert self.is_equal(solution(head, k), result)
+
+    def is_equal(self, l1, l2):
+        while l1 and l2:
+            if l1.val != l2.val:
+                return False
+            l1 = l1.next
+            l2 = l2.next
+        return True
