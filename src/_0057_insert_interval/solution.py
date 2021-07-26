77533795ba2cf199db92a4facb637539570b16c4
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-26 11:21:48 +0530

    :sparkles: Add solution and testcases to problem 57

--- src/_0057_insert_interval/solution.py
+++ src/_0057_insert_interval/solution.py
@@ -1,3 +1,24 @@
-def solution():
-return
-    
+from typing import List
+
+
+def solution(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    x, y = newInterval
+    if not intervals:
+        return [[x, y]]
+    new_intervals = []
+    for i in range(len(intervals)):
+        if intervals[i][0] <= x:
+            new_intervals.append(intervals[i])
+        else:
+            break
+    new_intervals.append([x, y])
+    for i in range(i, len(intervals)):
+        new_intervals.append(intervals[i])
+
+    intervals = []
+    for x, y in new_intervals:
+        if not intervals or intervals[-1][1] < x:
+            intervals.append([x, y])
+        else:
+            intervals[-1][1] = max(intervals[-1][1], y)
+    return intervals
--- tests/test_0057_insert_interval.py
+++ tests/test_0057_insert_interval.py
@@ -1,6 +1,17 @@
 import pytest
+from src._0057_insert_interval.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "intervals, new_interval, result",
+        [
+            ([[1, 3], [6, 9]], [2, 5], [[1, 5], [6, 9]]),
+            ([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8], [[1, 2], [3, 10], [12, 16]]),
+            ([], [5, 7], [[5, 7]]),
+            ([[1, 5]], [2, 3], [[1, 5]]),
+            ([[1, 5]], [2, 7], [[1, 7]]),
+        ],
+    )
+    def test_solution(self, intervals, new_interval, result):
+        assert solution(intervals, new_interval) == result
