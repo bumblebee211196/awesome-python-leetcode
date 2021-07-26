869e5e9186c0106b7a422b0dc38251ca21e3e522
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-26 15:12:50 +0530

    :sparkles: Add solution and testcases to problem 59

--- src/_0059_spiral_matrix_ii/solution.py
+++ src/_0059_spiral_matrix_ii/solution.py
@@ -1,2 +1,24 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(n: int) -> List[List[int]]:
+    matrix = [[0 for _ in range(n)] for _ in range(n)]
+    x0 = y0 = 0
+    x1 = y1 = n
+    values = [v for v in range(1, (n * n) + 1)][::-1]
+    while y1 > y0 and x1 > x0:
+        for i in range(x0, x1):
+            matrix[y0][i] = values.pop()
+        for j in range(y0 + 1, y1 - 1):
+            matrix[j][x1 - 1] = values.pop()
+        if y1 != y0 + 1:
+            for i in range(x1 - 1, x0 - 1, -1):
+                matrix[y1 - 1][i] = values.pop()
+        if x0 != x1 - 1:
+            for j in range(y1 - 2, y0, -1):
+                matrix[j][x0] = values.pop()
+        y0 += 1
+        y1 -= 1
+        x0 += 1
+        x1 -= 1
+    return matrix
--- tests/test_0059_spiral_matrix_ii.py
+++ tests/test_0059_spiral_matrix_ii.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0059_spiral_matrix_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "n, result",
+        [
+            (1, [[1]]),
+            (3, [[1, 2, 3], [8, 9, 4], [7, 6, 5]]),
+        ],
+    )
+    def test_solution(self, n, result):
+        assert solution(n) == result
