28e457b25b23b48758e7c95f2f5e9565272dc336
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:56:03 +0530

    :sparkles: Add solution and testcases to problem 70

--- src/_0070_climbing_stairs/solution.py
+++ src/_0070_climbing_stairs/solution.py
@@ -1,2 +1,7 @@
-def solution():
-    return
+def solution(n: int) -> int:
+    if n <= 2:
+        return n
+    p1, p2 = 1, 0
+    for _ in range(n):
+        p1, p2 = p1 + p2, p1
+    return p1
--- tests/test_0070_climbing_stairs.py
+++ tests/test_0070_climbing_stairs.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0070_climbing_stairs.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "n, result",
+        [
+            (2, 2),
+            (3, 3),
+            (4, 5),
+            (5, 8),
+        ],
+    )
+    def test_solution(self, n, result):
+        assert solution(n) == result
