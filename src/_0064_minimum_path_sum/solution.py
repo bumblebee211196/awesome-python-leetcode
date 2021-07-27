53e8fe6b8b8918163588dd38e3679b605421afdc
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:23:20 +0530

    :sparkles: Add solution and testcases to problem 64

--- src/_0064_minimum_path_sum/solution.py
+++ src/_0064_minimum_path_sum/solution.py
@@ -1,2 +1,13 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(grid: List[List[int]]) -> int:
+    n, m = len(grid), len(grid[0])
+    for j in range(1, m):
+        grid[0][j] += grid[0][j - 1]
+    for i in range(1, n):
+        grid[i][0] += grid[i - 1][0]
+    for i in range(1, n):
+        for j in range(1, m):
+            grid[i][j] += min(grid[i][j - 1], grid[i - 1][j])
+    return grid[-1][-1]
--- tests/test_0064_minimum_path_sum.py
+++ tests/test_0064_minimum_path_sum.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0064_minimum_path_sum.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "grid, result",
+        [
+            ([[1, 3, 1], [1, 5, 1], [4, 2, 1]], 7),
+            ([[1, 2, 3], [4, 5, 6]], 12),
+        ],
+    )
+    def test_solution(self, grid, result):
+        assert solution(grid) == result
