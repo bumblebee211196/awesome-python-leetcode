cbe8077128e83da7bb9009825ae2a5b7415c383b
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:28:47 +0530

    :sparkles: Add solution and testcases to problem 66

--- src/_0066_plus_one/solution.py
+++ src/_0066_plus_one/solution.py
@@ -1,2 +1,11 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(digits: List[int]) -> List[int]:
+    carry = 1
+    for i in range(len(digits) - 1, -1, -1):
+        carry, r = divmod(digits[i] + carry, 10)
+        digits[i] = r
+    if carry > 0:
+        digits = [carry] + digits
+    return digits
--- tests/test_0066_plus_one.py
+++ tests/test_0066_plus_one.py
@@ -1,6 +1,10 @@
 import pytest
+from src._0066_plus_one.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "digits, result", [([1, 2, 3], [1, 2, 4]), ([9, 9], [1, 0, 0]), ([4, 3, 2, 1], [4, 3, 2, 2]), ([0], [1])]
+    )
+    def test_solution(self, digits, result):
+        assert solution(digits) == result
