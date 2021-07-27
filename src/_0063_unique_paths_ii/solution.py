c16062cf488b40dcbcb28e09393c8e6b121eecff
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:21:18 +0530

    :sparkles: Add solution and testcases to problem 63

--- src/_0063_unique_paths_ii/solution.py
+++ src/_0063_unique_paths_ii/solution.py
@@ -1,2 +1,14 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(obstacle_grid: List[List[int]]) -> int:
+    if 1 in (obstacle_grid[0][0], obstacle_grid[-1][-1]):
+        return 0
+    n, m = len(obstacle_grid), len(obstacle_grid[0])
+    matrix = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    matrix[1][1] = 1
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if obstacle_grid[i - 1][j - 1] == 0:
+                matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1]
+    return matrix[-1][-1]
--- tests/test_0063_unique_paths_ii.py
+++ tests/test_0063_unique_paths_ii.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0063_unique_paths_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "obstacle_grid, result",
+        [
+            ([[0, 0, 0], [0, 1, 0], [0, 0, 0]], 2),
+            ([[0, 1], [0, 0]], 1),
+            ([[1, 1], [0, 0]], 0),
+        ],
+    )
+    def test_solution(self, obstacle_grid, result):
+        assert solution(obstacle_grid) == result
