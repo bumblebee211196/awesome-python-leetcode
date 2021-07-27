c8af0dacc46d21261df19d35b5b8ec05a35620cb
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:37:59 +0530

    :sparkles: Add solution and testcases to problem 68

--- src/_0068_text_justification/solution.py
+++ src/_0068_text_justification/solution.py
@@ -1,2 +1,23 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(words: List[str], k: int) -> List[str]:
+    result = []
+    curr_words_len = 0
+    curr_words = []
+    for word in words:
+        if curr_words_len + len(curr_words) + len(word) > k:
+            if len(curr_words) == 1:
+                result.append(curr_words[0] + " " * (k - curr_words_len))
+            else:
+                num_spaces = k - curr_words_len
+                spaces_between, extras = divmod(num_spaces, len(curr_words) - 1)
+                for i in range(extras):
+                    curr_words[i] += " "
+                result.append((" " * spaces_between).join(curr_words))
+            curr_words = []
+            curr_words_len = 0
+        curr_words.append(word)
+        curr_words_len += len(word)
+    result.append(" ".join(curr_words) + " " * (k - curr_words_len - len(curr_words) + 1))
+    return result
--- tests/test_0068_text_justification.py
+++ tests/test_0068_text_justification.py
@@ -1,6 +1,53 @@
 import pytest
+from src._0068_text_justification.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "words, k, result",
+        [
+            (
+                ["This", "is", "an", "example", "of", "text", "justification."],
+                16,
+                ["This    is    an", "example  of text", "justification.  "],
+            ),
+            (
+                ["What", "must", "be", "acknowledgment", "shall", "be"],
+                16,
+                ["What   must   be", "acknowledgment  ", "shall be        "],
+            ),
+            (
+                [
+                    "Science",
+                    "is",
+                    "what",
+                    "we",
+                    "understand",
+                    "well",
+                    "enough",
+                    "to",
+                    "explain",
+                    "to",
+                    "a",
+                    "computer.",
+                    "Art",
+                    "is",
+                    "everything",
+                    "else",
+                    "we",
+                    "do",
+                ],
+                20,
+                [
+                    "Science  is  what we",
+                    "understand      well",
+                    "enough to explain to",
+                    "a  computer.  Art is",
+                    "everything  else  we",
+                    "do                  ",
+                ],
+            ),
+        ],
+    )
+    def test_solution(self, words, k, result):
+        assert solution(words, k) == result
