46ea197fe27370f218a40dd0dc36aefe51a79718
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:50:18 +0530

    :sparkles: Add solution and testcases to problem 69

--- src/_0069_sqrtx/solution.py
+++ src/_0069_sqrtx/solution.py
@@ -1,2 +1,13 @@
-def solution():
-    return
+def solution(x: int) -> int:
+    if x == 1:
+        return 1
+    l, r = 1, x // 2
+    while l <= r:
+        m = (l + r) // 2
+        if m * m == x:
+            return m
+        elif m * m > x:
+            r = m - 1
+        else:
+            l = m + 1
+    return (l + r) // 2
--- tests/test_0069_sqrtx.py
+++ tests/test_0069_sqrtx.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0069_sqrtx.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "x, result",
+        [
+            (4, 2),
+            (1, 1),
+            (8, 2),
+        ],
+    )
+    def test_solution(self, x, result):
+        assert solution(x) == result
