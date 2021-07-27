ac22328d3bd879de0b07dad23e8c9813d7354619
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 11:13:38 +0530

    :sparkles: Add solution and testcases to problem 62

--- src/_0062_unique_paths/solution.py
+++ src/_0062_unique_paths/solution.py
@@ -1,2 +1,7 @@
-def solution():
-    return
+def solution(m: int, n: int) -> int:
+    matrix = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+    matrix[1][1] = 1
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1]
+    return matrix[-1][-1]
--- tests/test_0062_unique_paths.py
+++ tests/test_0062_unique_paths.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0062_unique_paths.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "m, n, result",
+        [
+            (3, 7, 28),
+            (3, 2, 3),
+            (7, 3, 28),
+            (3, 3, 6),
+        ],
+    )
+    def test_solution(self, m, n, result):
+        assert solution(m, n) == result
