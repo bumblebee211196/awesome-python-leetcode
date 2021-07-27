3e236863782c3f4e4e68f65232a53d7336247132
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-27 22:34:40 +0530

    :sparkles: Add solution and testcases to problem 67

--- src/_0067_add_binary/solution.py
+++ src/_0067_add_binary/solution.py
@@ -1,2 +1,23 @@
-def solution():
-    return
+def solution(a: str, b: str) -> str:
+    a = list(map(int, list(a)))
+    b = list(map(int, list(b)))
+    res = []
+    la, lb = len(a), len(b)
+    i, j = la - 1, lb - 1
+    carry = 0
+    while i >= 0 and j >= 0:
+        carry, r = divmod(a[i] + b[j] + carry, 2)
+        res.append(r)
+        i -= 1
+        j -= 1
+    while i >= 0:
+        carry, r = divmod(a[i] + carry, 2)
+        res.append(r)
+        i -= 1
+    while j >= 0:
+        carry, r = divmod(b[j] + carry, 2)
+        res.append(r)
+        j -= 1
+    if carry > 0:
+        res.append(carry)
+    return "".join(list(map(str, res[::-1])))
--- tests/test_0067_add_binary.py
+++ tests/test_0067_add_binary.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0067_add_binary.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "a, b, result",
+        [
+            ("11", "1", "100"),
+            ("1", "11", "100"),
+            ("1010", "1011", "10101"),
+        ],
+    )
+    def test_solution(self, a, b, result):
+        assert solution(a, b) == result
