4d6f7703441e3f9f10456194e99e0b44ab93454f
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:33:43 +0530

    :sparkles: Add solution and testcases to problem 77

--- src/_0077_combinations/solution.py
+++ src/_0077_combinations/solution.py
@@ -1,2 +1,17 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(n: int, k: int) -> List[List[int]]:
+    def helper(idx, curr):
+        if len(curr) == k:
+            nonlocal res
+            res.append(curr[:])
+            return
+        for i in range(idx, n + len(curr) - k + 2):
+            curr.append(i)
+            helper(i + 1, curr)
+            curr.pop()
+
+    res = []
+    helper(1, [])
+    return res
--- tests/test_0077_combinations.py
+++ tests/test_0077_combinations.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0077_combinations.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "n, k, result",
+        [
+            (4, 2, [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]),
+            (1, 1, [[1]]),
+        ],
+    )
+    def test_solution(self, n, k, result):
+        assert solution(n, k) == result
