15510789e3931d2d6b96db0d4feeb2dbbe8ae1fd
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:05:43 +0530

    :sparkles: Add solution and testcases to problem 73

--- src/_0073_set_matrix_zeroes/solution.py
+++ src/_0073_set_matrix_zeroes/solution.py
@@ -1,2 +1,20 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(matrix: List[List[int]]) -> None:
+    rows, cols = [], []
+    m, n = len(matrix), len(matrix[0])
+    to_set_zeros = []
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 0:
+                to_set_zeros.append([i, j])
+    for i, j in to_set_zeros:
+        if j not in cols:
+            for i_ in range(m):
+                matrix[i_][j] = 0
+            cols.append(j)
+        if i not in rows:
+            for j_ in range(n):
+                matrix[i][j_] = 0
+            rows.append(i)
--- tests/test_0073_set_matrix_zeroes.py
+++ tests/test_0073_set_matrix_zeroes.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0073_set_matrix_zeroes.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "matrix, result",
+        [
+            ([[1, 1, 1], [1, 0, 1], [1, 1, 1]], [[1, 0, 1], [0, 0, 0], [1, 0, 1]]),
+            ([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]], [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]),
+        ],
+    )
+    def test_solution(self, matrix, result):
+        solution(matrix)
+        assert matrix == result
