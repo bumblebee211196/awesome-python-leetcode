fe150ddf8ceccb8e8f6726ec3c6ea63267d164c0
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:00:41 +0530

    :sparkles: Add solution and testcases to problem 71

--- src/_0071_simplify_path/solution.py
+++ src/_0071_simplify_path/solution.py
@@ -1,2 +1,12 @@
-def solution():
-    return
+def solution(path: str) -> str:
+    paths = path.split("/")
+    stack = []
+    for p in paths:
+        if p in ("", "."):
+            continue
+        elif p == "..":
+            if stack:
+                stack.pop()
+        else:
+            stack.append(p)
+    return "/" + "/".join(stack)
--- tests/test_0071_simplify_path.py
+++ tests/test_0071_simplify_path.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0071_simplify_path.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "path, result",
+        [
+            ("/home/", "/home"),
+            ("/../", "/"),
+            ("/home//foo/", "/home/foo"),
+            ("/a/./b/../../c/", "/c"),
+        ],
+    )
+    def test_solution(self, path, result):
+        assert solution(path) == result
