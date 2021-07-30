35f232e17d15159275398f09f455de450a3eb5ee
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:41:38 +0530

    :sparkles: Add solution and testcases to problem 78

--- src/_0078_subsets/solution.py
+++ src/_0078_subsets/solution.py
@@ -1,2 +1,16 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(nums: List[int]) -> List[List[int]]:
+    def helper(idx, curr):
+        nonlocal res
+        res.append(curr[:])
+        for i in range(idx, len(nums)):
+            curr.append(nums[i])
+            helper(i + 1, curr)
+            curr.pop()
+
+    res = []
+    helper(0, [])
+    print(res)
+    return res
--- tests/test_0078_subsets.py
+++ tests/test_0078_subsets.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0078_subsets.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([1, 2, 3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]),
+            ([0], [[], [0]]),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
