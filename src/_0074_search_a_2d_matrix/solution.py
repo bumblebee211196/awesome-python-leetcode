8b906938473a1fb0de9c26c937318a1e5eec1cb8
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:19:12 +0530

    :sparkles: Add solution and testcases to problem 74

--- src/_0074_search_a_2d_matrix/solution.py
+++ src/_0074_search_a_2d_matrix/solution.py
@@ -1,2 +1,19 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(matrix: List[List[int]], target: int) -> bool:
+    m = len(matrix[0])
+    for row in matrix:
+        if target > row[-1]:
+            continue
+        else:
+            l, r = 0, m - 1
+            while l <= r:
+                mid = (l + r) // 2
+                if row[mid] == target:
+                    return True
+                elif row[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return False
--- tests/test_0074_search_a_2d_matrix.py
+++ tests/test_0074_search_a_2d_matrix.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0074_search_a_2d_matrix.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "matrix, target, result",
+        [
+            ([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3, True),
+            ([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13, False),
+        ],
+    )
+    def test_solution(self, matrix, target, result):
+        assert solution(matrix, target) == result
