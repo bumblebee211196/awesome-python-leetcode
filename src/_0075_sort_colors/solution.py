4aa5b405a794ef8be6293f7b8c94558c5a6b74d7
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 11:28:22 +0530

    :sparkles: Add solution and testcases to problem 75

--- src/_0075_sort_colors/solution.py
+++ src/_0075_sort_colors/solution.py
@@ -1,2 +1,16 @@
-def solution():
+from typing import List
+
+
+def solution(nums: List[int]) -> None:
+    l, m, r = 0, 0, len(nums) - 1
+    while m <= r:
+        if nums[m] == 0:
+            nums[l], nums[m] = nums[m], nums[l]
+            l += 1
+            m += 1
+        elif nums[m] == 1:
+            m += 1
+        elif nums[m] == 2:
+            nums[m], nums[r] = nums[r], nums[m]
+            r -= 1
     return
--- tests/test_0075_sort_colors.py
+++ tests/test_0075_sort_colors.py
@@ -1,6 +1,17 @@
 import pytest
+from src._0075_sort_colors.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([2, 0, 2, 1, 1, 0], [0, 0, 1, 1, 2, 2]),
+            ([2, 0, 1], [0, 1, 2]),
+            ([0], [0]),
+            ([1], [1]),
+        ],
+    )
+    def test_solution(self, nums, result):
+        solution(nums)
+        assert nums == result
