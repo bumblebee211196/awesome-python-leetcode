9f90f2464bcbfefd99faa1b11ea08d7ced55d466
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 12:38:38 +0530

    :sparkles: Add solution and testcases to problem 79

--- src/_0079_word_search/solution.py
+++ src/_0079_word_search/solution.py
@@ -1,2 +1,23 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(board: List[List[str]], word: str) -> bool:
+    def helper(r, c, word, matrix):
+        if not word:
+            return True
+        if 0 <= r < n and 0 <= c < m and matrix[r][c] == word[-1]:
+            matrix[r][c] = "_"
+            return (
+                helper(r + 1, c, word[:-1], matrix)
+                or helper(r - 1, c, word[:-1], matrix)
+                or helper(r, c + 1, word[:-1], matrix)
+                or helper(r, c - 1, word[:-1], matrix)
+            )
+        return False
+
+    n, m = len(board), len(board[0])
+    for i in range(n):
+        for j in range(m):
+            if helper(i, j, word, board):
+                return True
+    return False
--- tests/test_0079_word_search.py
+++ tests/test_0079_word_search.py
@@ -1,6 +1,15 @@
 import pytest
+from src._0079_word_search.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "board, word, result",
+        [
+            ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED", True),
+            ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE", True),
+            ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB", False),
+        ],
+    )
+    def test_solution(self, board, word, result):
+        assert solution(board, word) == result
