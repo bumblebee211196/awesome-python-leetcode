d7d1fe838f615b8ba6109ca119be693d6e934ba2
S Sathish Babu <sathish.babu@zohocorp.com>
2021-07-30 12:44:17 +0530

    :sparkles: Add solution and testcases to problem 80

--- src/_0080_remove_duplicates_from_sorted_array_ii/solution.py
+++ src/_0080_remove_duplicates_from_sorted_array_ii/solution.py
@@ -1,2 +1,27 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(nums: List[int]) -> int:
+    count = 0
+    prev = None
+    l = r = 0
+    while r < len(nums):
+        if prev is None:
+            prev = nums[r]
+            count = 1
+        elif prev == nums[r]:
+            count += 1
+        else:
+            count = 2 if count > 2 else count
+            for _ in range(count):
+                nums[l] = prev
+                l += 1
+            prev = nums[r]
+            count = 1
+        r += 1
+    count = 2 if count > 2 else count
+    if prev is not None:
+        for _ in range(count):
+            nums[l] = prev
+            l += 1
+    return l
--- tests/test_0080_remove_duplicates_from_sorted_array_ii.py
+++ tests/test_0080_remove_duplicates_from_sorted_array_ii.py
@@ -1,6 +1,14 @@
 import pytest
+from src._0080_remove_duplicates_from_sorted_array_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, result",
+        [
+            ([1, 1, 1, 2, 2, 3], 5),
+            ([0, 0, 1, 1, 1, 1, 2, 3, 3], 7),
+        ],
+    )
+    def test_solution(self, nums, result):
+        assert solution(nums) == result
