28f74dc818ee91263b5f70149ba57344bb8a4de0
bumblebebee211196 <bumblebeee211196@gmail.com>
2021-08-08 14:28:30 +0530

    :sparkles: Add solution and testcases to problem 86

--- src/_0086_partition_list/solution.py
+++ src/_0086_partition_list/solution.py
@@ -1,2 +1,24 @@
-def solution():
-    return
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
+def solution(head: Optional[ListNode], x: int) -> Optional[ListNode]:
+    left = ListNode(-999)
+    right = ListNode(-999)
+    l, r, curr = left, right, head
+    while curr:
+        if curr.val < x:
+            l.next = ListNode(curr.val)
+            l = l.next
+        else:
+            r.next = ListNode(curr.val)
+            r = r.next
+        curr = curr.next
+    left, right = left.next, right.next
+    l.next = right
+    return left if left else right
--- tests/test_0086_partition_list.py
+++ tests/test_0086_partition_list.py
@@ -1,6 +1,28 @@
 import pytest
+from src._0086_partition_list.solution import ListNode, solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "head, x, result",
+        [
+            (
+                ListNode(1, ListNode(4, ListNode(3, ListNode(2, ListNode(5, ListNode(2)))))),
+                3,
+                ListNode(1, ListNode(2, ListNode(2, ListNode(4, ListNode(3, ListNode(5)))))),
+            ),
+            (ListNode(2, ListNode(1)), 2, ListNode(1, ListNode(2))),
+            (ListNode(1), 0, ListNode(1)),
+        ],
+    )
+    def test_solution(self, head, x, result):
+        assert self.is_equal(solution(head, x), result)
+
+    def is_equal(self, head1, head2):
+        c1, c2 = head1, head2
+        while c1 and c2:
+            if c1.val != c2.val:
+                return False
+            c1 = c1.next
+            c2 = c2.next
+        return True
