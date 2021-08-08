de4eb24f1d7920834cec687594f1af4ac32c0c9b
bumblebebee211196 <bumblebeee211196@gmail.com>
2021-08-08 13:46:39 +0530

    :sparkles: Add solution and testcases to problem 81

--- src/_0081_search_in_rotated_sorted_array_ii/solution.py
+++ src/_0081_search_in_rotated_sorted_array_ii/solution.py
@@ -1,2 +1,28 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(nums: List[int], target: int) -> bool:
+    new_nums = []
+    seen = set()
+    for num in nums:
+        if num not in seen:
+            new_nums.append(num)
+            seen.add(num)
+    nums = new_nums
+    n = len(nums)
+    l, r = 0, n - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return True
+        elif nums[m] >= nums[l]:
+            if nums[l] <= target <= nums[m]:
+                r = m - 1
+            else:
+                l = m + 1
+        else:
+            if nums[m] <= target <= nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+    return False
--- tests/test_0081_search_in_rotated_sorted_array_ii.py
+++ tests/test_0081_search_in_rotated_sorted_array_ii.py
@@ -1,6 +1,17 @@
 import pytest
+from src._0081_search_in_rotated_sorted_array_ii.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums, target, result",
+        [
+            ([2, 5, 6, 0, 0, 1, 2], 0, True),
+            ([2, 5, 6, 0, 0, 1, 2], 3, False),
+            ([4, 5, 6, 7, 0, 0, 2], 0, True),
+            ([2, 5, 6, 7, 0, 1, 2], 2, True),
+            ([5, 5, 6, 0, 1, 2, 3], 5, True),
+        ],
+    )
+    def test_solution(self, nums, target, result):
+        assert solution(nums, target) == result
