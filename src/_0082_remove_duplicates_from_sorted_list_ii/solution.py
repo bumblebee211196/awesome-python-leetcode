f709c19cb5a188792404af6a852a32cd611f3ab6
bumblebebee211196 <bumblebeee211196@gmail.com>
2021-08-08 14:03:03 +0530

    :sparkles: Add solution and testcases to problem 82

--- src/_0082_remove_duplicates_from_sorted_list_ii/solution.py
+++ src/_0082_remove_duplicates_from_sorted_list_ii/solution.py
@@ -1,2 +1,30 @@
-def solution():
-    return
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def solution(head: Optional[ListNode]) -> Optional[ListNode]:
+    seen = set()
+    to_remove = set()
+    curr = head
+    while curr:
+        if curr.val not in seen:
+            seen.add(curr.val)
+        else:
+            to_remove.add(curr.val)
+        curr = curr.next
+    dummy = ListNode(-999)
+    p1, p2 = dummy, head
+    p2_next = None
+    while p2:
+        p2_next = p2.next
+        p2.next = None
+        if p2.val not in to_remove:
+            p1.next = p2
+            p1 = p1.next
+        p2 = p2_next
+    return dummy.next
--- tests/test_0082_remove_duplicates_from_sorted_list_ii.py
+++ tests/test_0082_remove_duplicates_from_sorted_list_ii.py
@@ -1,6 +1,27 @@
+from typing import List
 import pytest
+from src._0082_remove_duplicates_from_sorted_list_ii.solution import solution, ListNode
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "head, result",
+        [
+            (
+                ListNode(1, ListNode(3, ListNode(3, ListNode(4, ListNode(4, ListNode(5)))))),
+                ListNode(1, ListNode(2, ListNode(5))),
+            ),
+            (ListNode(1, ListNode(1, ListNode(1, ListNode(2, ListNode(3))))), ListNode(2, ListNode(3))),
+        ],
+    )
+    def test_solution(self, head, result):
+        assert self.is_equal(solution(head), result)
+
+    def is_equal(self, head1, head2):
+        c1, c2 = head1, head2
+        while c1 and c2:
+            if c1.val != c2.val:
+                return False
+            c1 = c1.next
+            c2 = c2.next
+        return True
