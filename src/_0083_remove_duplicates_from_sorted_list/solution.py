f14c7c09f1eab670cbfd4670eb1a6dd80183dbbe
bumblebebee211196 <bumblebeee211196@gmail.com>
2021-08-08 14:18:43 +0530

    :sparkles: Add solution and testcases to problem 83

--- src/_0083_remove_duplicates_from_sorted_list/solution.py
+++ src/_0083_remove_duplicates_from_sorted_list/solution.py
@@ -1,2 +1,19 @@
-def solution():
-    return
+class ListNode:
+    def __init__(self, x, next_=None):
+        self.val = x
+        self.next = next_
+
+
+def solution(head: ListNode) -> ListNode:
+    prev = head
+    curr = head
+    while curr:
+        if prev.val == curr.val:
+            curr = curr.next
+        else:
+            prev.next = curr
+            prev = curr
+            curr = curr.next
+    if prev:
+        prev.next = None
+    return head
--- tests/test_0083_remove_duplicates_from_sorted_list.py
+++ tests/test_0083_remove_duplicates_from_sorted_list.py
@@ -1,6 +1,23 @@
 import pytest
+from src._0083_remove_duplicates_from_sorted_list.solution import ListNode, solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "head, result",
+        [
+            (ListNode(1, ListNode(1, ListNode(2))), ListNode(1, ListNode(2))),
+            (ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3))))), ListNode(1, ListNode(2, ListNode(3)))),
+        ],
+    )
+    def test_solution(self, head, result):
+        assert self.is_equal(solution(head), result)
+
+    def is_equal(self, head1, head2):
+        c1, c2 = head1, head2
+        while c1 and c2:
+            if c1.val != c2.val:
+                return False
+            c1 = c1.next
+            c2 = c2.next
+        return True
