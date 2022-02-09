1ef12f6271809f8853d31272f864d1f6fe23728b
S Sathish Babu <sathish.babu@zohocorp.com>
2022-02-09 15:14:57 +0530

    ✨ Add solution and testcases to problem 88

--- src/_0088_merge_sorted_array/solution.py
+++ src/_0088_merge_sorted_array/solution.py
@@ -1,2 +1,22 @@
-def solution():
-    return
+from typing import List
+
+
+def solution(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    i, j = m - 1, n - 1
+    k = m + n - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+    while i >= 0:
+        nums1[k] = nums1[i]
+        i -= 1
+        k -= 1
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
--- tests/test_0088_merge_sorted_array.py
+++ tests/test_0088_merge_sorted_array.py
@@ -1,6 +1,16 @@
 import pytest
+from src._0088_merge_sorted_array.solution import solution
 
 
 class TestSolution:
-    def test_solution(self):
-        assert True
+    @pytest.mark.parametrize(
+        "nums1, m, nums2, n, result",
+        [
+            ([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3, [1, 2, 2, 3, 5, 6]),
+            ([1], 1, [], 0, [1]),
+            ([0], 0, [1], 1, [1]),
+        ],
+    )
+    def test_solution(self, nums1, m, nums2, n, result):
+        solution(nums1, m, nums2, n)
+        assert nums1 == result
